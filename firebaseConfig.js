d81e4dd5231a4bb303084b7a8a3c1ed3e0e1d0a9
Malkaviala <128104470+Malkaviala@users.noreply.github.com>
2024-09-27 10:16:57 -0400

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+// firebaseConfig.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDTutCGdUeopdBpR-yYKdOn8UXwy1UQ1t8",
+    authDomain: "fuel-f60f7.firebaseapp.com",
+    databaseURL: "https://fuel-f60f7-default-rtdb.firebaseio.com",
+    projectId: "fuel-f60f7",
+    storageBucket: "fuel-f60f7.appspot.com",
+    messagingSenderId: "286098349793",
+    appId: "1:286098349793:web:182f10dc0fea18423c21a0",
+    measurementId: "G-4N6DZ5FTKM"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
