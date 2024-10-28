6c480e13be63a9b3f16ae409a2b1ea3d0779d8cc
Malkaviala <128104470+Malkaviala@users.noreply.github.com>
2024-10-28 19:55:16 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,12 @@
 // firebaseConfig.js
 const firebaseConfig = {
-    apiKey: "AIzaSyDTutCGdUeopdBpR-yYKdOn8UXwy1UQ1t8",
-    authDomain: "fuel-f60f7.firebaseapp.com",
-    databaseURL: "https://fuel-f60f7-default-rtdb.firebaseio.com",
-    projectId: "fuel-f60f7",
-    storageBucket: "fuel-f60f7.appspot.com",
-    messagingSenderId: "286098349793",
-    appId: "1:286098349793:web:182f10dc0fea18423c21a0",
-    measurementId: "G-4N6DZ5FTKM"
+   apiKey: "AIzaSyAwgVoCZi4QZYxNZB-DElzh3OzXyPi_oWQ",
+  authDomain: "finance-c000b.firebaseapp.com",
+  projectId: "finance-c000b",
+  storageBucket: "finance-c000b.appspot.com",
+  messagingSenderId: "560534350244",
+  appId: "1:560534350244:web:64e1194ca97e41d9a97931",
+  measurementId: "G-BD3WQJ1FCY"
 };
 
 // Initialize Firebase
