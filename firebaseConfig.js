2966bcbe800e2f61b4ddfb08128a0faf3b302226
Malkaviala <128104470+Malkaviala@users.noreply.github.com>
2024-10-28 20:00:23 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,13 @@
 // firebaseConfig.js
 const firebaseConfig = {
-   apiKey: "AIzaSyAwgVoCZi4QZYxNZB-DElzh3OzXyPi_oWQ",
-  authDomain: "finance-c000b.firebaseapp.com",
-  projectId: "finance-c000b",
-  storageBucket: "finance-c000b.appspot.com",
-  messagingSenderId: "560534350244",
-  appId: "1:560534350244:web:64e1194ca97e41d9a97931",
+  apiKey: "AIzaSyBVH4syVcOVAJJI0BkPl8EWUcarmgqQIH0",
+  authDomain: "shipment-c50a1.firebaseapp.com",
+  databaseURL: "https://shipment-c50a1-default-rtdb.firebaseio.com",
+  projectId: "shipment-c50a1",
+  storageBucket: "shipment-c50a1.appspot.com",
+  messagingSenderId: "552502680496",
+  appId: "1:552502680496:web:5563feea124f54904d5475",
+  measurementId: "G-6C927VG178"e41d9a97931",
   measurementId: "G-BD3WQJ1FCY"
 };
 
